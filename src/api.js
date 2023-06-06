d0f5f41f93613f03b598c345d441ee7bfa53e0cf
zhuth <thzhu@139.com>
2023-06-06 16:53:21 +0800

    extension name fix

--- src/api.js
+++ src/api.js
@@ -622,7 +622,7 @@ const apis = {
   get_image_url(src) {
     var path = ''
     if (src.file.indexOf('://') >= 0) {
-      let ext = (src.url || src.orig_path || '').split('.').pop().split('?')[0]
+      let ext = (src.url || src.orig_path || '').split('?')[0].split('.').pop()
       let segs = src.file.split('://')
       path = `/images/${segs[0]}/${segs[1].replace('$', src.block_id) || src.block_id}/image.${ext.length <= 4 ? ext : 'data'}`
     }
